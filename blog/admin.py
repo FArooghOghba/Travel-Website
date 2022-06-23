22fc9473c4ba5acc3144b9a8b8f36e8d4a14e95a
FArooghOghba <faroogh.oghba@gmail.com>
2022-06-23 15:10:14 +0430

    Register & customize admin panel  for Post model in blog app

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Post
+
 
 # Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = [
+        'title',
+        'counted_views',
+        'status',
+        'publish_date',
+        'created_date'
+    ]
+    search_fields = ['title']
+    list_filter = ['status']
--- blog/models.py
+++ blog/models.py
@@ -14,3 +14,9 @@ class Post(models.Model):
     publish_date = models.DateTimeField(null=True)
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created_date']
+
+    def __str__(self):
+        return self.title
