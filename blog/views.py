be9b6823e3f77eb48692deae92ddc040c3fa1d07
FArooghOghba <faroogh.oghba@gmail.com>
2022-06-26 14:58:51 +0430

    add prev & next post in blog detail template.

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
-from django.core.paginator import Paginator
 
 from blog.models import Post
 
@@ -15,9 +14,29 @@ def blog_home_view(request):
 
 
 def blog_single_view(request, post_id):
-    post = get_object_or_404(Post, pk=post_id, publish_date__lte=timezone.now(), status=True)
-    post.counted_views += 1
-    post.save()
+    current_post = get_object_or_404(Post, pk=post_id, publish_date__lte=timezone.now(), status=True)
+    current_post.counted_views += 1
+    current_post.save()
 
-    context = {'post': post}
+    # prev_post = Post.objects.filter(             # find prev & next page with django orm.
+    #     publish_date__lte=timezone.now(),
+    #     status=True,
+    #     publish_date__lt=current_post.publish_date,
+    # ).first()
+    # next_post = Post.objects.filter(
+    #     publish_date__lte=timezone.now(),
+    #     status=True,
+    #     publish_date__gt=current_post.publish_date
+    # ).first()
+
+    # find prev & next page with list comprehension.
+    all_post = Post.objects.filter(publish_date__lte=timezone.now(), status=True)
+    prev_post = [post for post in all_post if post.publish_date < current_post.publish_date]
+    next_post = [post for post in list(reversed(all_post)) if post.publish_date > current_post.publish_date]
+
+    context = {
+        'current_post': current_post,
+        'next_post': next_post,
+        'prev_post': prev_post
+    }
     return render(request, template_name='blog/blog-single.html', context=context)
