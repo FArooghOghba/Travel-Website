b881b47e9b0a3457950a260065d3f69144411eb7
FArooghOghba <faroogh.oghba@gmail.com>
2022-06-28 17:44:33 +0430

    add post_categories in blog_tags & blog-home

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,6 +1,8 @@
+from blog.models import *
+
 from django import template
 from django.utils import timezone
-from blog.models import Post
+from django.db.models.aggregates import Count
 
 register = template.Library()
 
@@ -9,3 +11,19 @@ register = template.Library()
 def latest_posts():
     posts = Post.objects.filter(publish_date__lte=timezone.now(), status=True)[:4]
     return {'posts': posts}
+
+
+@register.inclusion_tag('blog/blog-post-category.html')
+def post_categories():
+    # result = Category.objects.annotate(post_count=Count('post'))
+    # for cat in result:
+    #     print(cat, cat.post_count)
+
+    categories = Category.objects.all()
+    posts = Post.objects.filter(publish_date__lte=timezone.now(), status=True)
+
+    post_categories_count = {}
+    for category in categories:
+        post_categories_count[category.name] = posts.filter(category=category).count()
+
+    return {'post_cats_count': post_categories_count}
